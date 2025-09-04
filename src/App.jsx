97c474c149bc16218041e7b113c46b02ddf4a66f
fathanhafidz <marblong29@gmail.com>
2025-09-04 08:25:10 +0700

    router dengan protected route

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,35 @@ import "./App.css";
 import { BrowserRouter ,Routes, Route } from "react-router-dom";
 import Frame from "./components/Sidebar/Frame";
 import Dashboard from "./components/Dashboard/index";
+import Auth from "./components/Auth";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
+function App(){
+  return(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/auth" element={<Auth/>}/>
+
+      <Route path="/*" element={
+        <ProtectedRoute>
+         <FrameLayout/>
+         </ProtectedRoute>
+         }
+         />
+    </Routes>
+  </BrowserRouter>
+  );
+};
+
+function FrameLayout() {
   return (
-    <BrowserRouter>
         <Frame>
             <Routes>
              <Route path="/" element={<Dashboard />} />
             </Routes>
         </Frame>
-    </BrowserRouter>
-
+   
   );
-}
+};
 
 export default App;
