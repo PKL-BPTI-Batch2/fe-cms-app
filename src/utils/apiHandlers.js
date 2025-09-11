904c43aba346158a9e5ac63e7dc3053c5bfc449e
fathanhafidz <marblong29@gmail.com>
2025-09-11 14:25:50 +0700

    menambahkan response error jika yg dikirim bukan message

--- src/utils/apiHandlers.js
+++ src/utils/apiHandlers.js
@@ -1,6 +1,8 @@
 //handle error
 export function handleApiError(error) {
-    return error.response?.data?.message || "terjadi kesalahan di server";
+    return error.response?.data?.message || 
+    error.response?.data?.error||
+     "terjadi kesalahan di server";
 };
 //handle succes
 export function handleApiSuccess(response, defaultMessage="berhasil") {
