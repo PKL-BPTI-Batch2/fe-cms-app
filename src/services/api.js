221d8ecfa7c197a069c05de7b9ab8337c6e68221
fathanhafidz <marblong29@gmail.com>
2025-09-24 09:42:07 +0700

    menggunakan cookie

--- src/services/api.js
+++ src/services/api.js
@@ -1,29 +1,21 @@
 import axios from "axios";
 
-
 const API = axios.create({
-    baseURL : import.meta.env.VITE_API_URL,
-});
-// Interceptor Request: Tambah token otomatis ke header
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true // supaya cookie ikut terkirim
 });
 
-// Interceptor Response: Handle error global
+
+
 API.interceptors.response.use(
   (res) => res,
   (error) => {
-    // Kalau token expired atau tidak valid
     if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      // Redirect ke halaman login
+      // kalau sesi tidak valid
       window.location.href = "/auth";
     }
     return Promise.reject(error);
   }
 );
+
 export default API;
