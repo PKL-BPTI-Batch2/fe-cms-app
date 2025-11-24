c5f0f197a6c37815aa9d5849a3a6423585302e64
Sabrina Qodri Nova <sabrinaaaaa51@gmail.com>
2025-11-24 12:05:08 +0700

    Merge: Resolved App.jsx for 5-add-feature-menu-management

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import "./App.css";
 import { BrowserRouter ,Routes, Route } from "react-router-dom";
 import Frame from "./components/Sidebar/Frame";
-<<<<<<< HEAD
-import Dashboard from "./components/Dashboard/index";
 import Auth from "./components/Auth";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { AppProvider } from "./components/AppContext";
 import News from "./components/News/News";
 import Users from "./components/Users/Users";
-=======
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
 import Menu from "./components/Menu/Menu.jsx";
 import MenuItemsPage from "./components/Menu/Menuitempage.jsx";
->>>>>>> origin/5-add-feature-menu-management
 
 function App(){
   return(
@@ -38,13 +34,10 @@ function FrameLayout() {
         <Frame>
             <Routes>
              <Route path="/" element={<Dashboard />} />
-<<<<<<< HEAD
              <Route path="/news" element={<News />} />
              <Route path="/users" element={<Users />} />
-=======
               <Route path="/menus" element={<Menu />} />
             
->>>>>>> origin/5-add-feature-menu-management
             </Routes>
         </Frame>
    
